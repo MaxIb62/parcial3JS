6e56165d60f8419f6047bb83eb9d3b3f1701e417
Max <cdmx2254@amerike.edu.mx>
2023-10-16 20:55:27 -0600

    cambios

---
+++ JavaScript/funcionbotones.js
@@ -0,0 +1,11 @@
+/*document.getElementById("boton").addEventListener("click", function() {
+    document.getElementById("parrafo").innerHTML = "Texto modificado";
+});
+
+document.getElementById("elemento1").addEventListener("mouseover", function() {
+    this.style.backgroundColor = "lightblue";
+});
+
+document.getElementById("elemento2").addEventListener("mouseout", function() {
+    this.style.backgroundColor = "white";
+});*/
